ce6642c2d63663e206ed8cd9f9a35ddfd36d8264
ladeirass <ladeira@tutanota.com>
2022-09-13 11:51:39 -0300

    Atualização

---
+++ Exercicio01/src/codigos/Adicao.java
@@ -0,0 +1,13 @@
+package codigos;
+
+public class Adicao {
+
+public static void main(String[] args) {
+    
+
+    
+}
+    
+}
+    
+
--- Exercicio01/src/codigos/JavaComoProgramar.java
+++ Exercicio01/src/codigos/JavaComoProgramar.java
@@ -5,7 +5,19 @@ public class JavaComoProgramar {
 	public static void main(String[] args) {
 		System.out.print("Bem vindo");
 		System.out.println("Java como progrmar!");
+		System.out.println("----------------------------");
+		// ---(\n) Nova linha
+		System.out.println("Bem vindo\nao\nJava\nComo Programar");
+		// ---(\t) Tabulação horizontal
+		System.out.println("Java\tComo\tProgrmar");
+		System.out.println("Java\rComo\rProgramar");
+		System.out.println("------------------------------");
 
+		System.out.printf("%s%n%s%n", "Bem vindo ao",
+				"Java como Progarmar");
 
-	} // fim do método main
+				
+	}
+
+	// fim do método main
 } // fim da classe Welcome1
