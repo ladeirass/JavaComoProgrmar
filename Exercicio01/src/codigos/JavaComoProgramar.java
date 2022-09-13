5c99898c67cd705110a301002bb2b09098186dd3
ladeirass <ladeira@tutanota.com>
2022-09-13 10:30:12 -0300

    Primeiro commit

---
+++ Exercicio01/src/codigos/JavaComoProgramar.java
@@ -0,0 +1,11 @@
+package codigos;
+
+public class JavaComoProgramar {
+
+	public static void main(String[] args) {
+		System.out.print("Bem vindo");
+		System.out.println("Java como progrmar!");
+
+
+	} // fim do método main
+} // fim da classe Welcome1
