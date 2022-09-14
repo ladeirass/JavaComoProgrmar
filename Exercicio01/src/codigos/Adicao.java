3b9d9b5152b63673683cf8cad77fddc97e9d2329
ladeirass <ladeira@tutanota.com>
2022-09-14 11:11:31 -0300

    Adição

--- Exercicio01/src/codigos/Adicao.java
+++ Exercicio01/src/codigos/Adicao.java
@@ -1,13 +1,29 @@
 package codigos;
 
+import java.util.Scanner;
+
 public class Adicao {
 
-public static void main(String[] args) {
-    
+    public static void main(String[] args) {
 
-    
-}
-    
-}
-    
+        Scanner leia = new Scanner(System.in);
 
+        int n1;
+        int n2;
+        int soma;
+
+        System.out.print("Entre com um número: ");
+        n1 = leia.nextInt();
+
+        System.out.print("Entre com  o segundo número: ");
+        n2 = leia.nextInt();
+
+        soma = n1 + n2;
+
+        System.out.printf("A soma dos numeros: %d%n", soma);
+
+        leia.close();
+
+    }
+
+}
